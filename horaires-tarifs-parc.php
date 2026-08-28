471d5d1ebae10e1bea833a571de3b8bb584fef1a
montagnedessinges <montagnedessinges@gmail.com>
2026-08-28 10:03:44 +0200

    1.9.3: activer tarifs par saison et offres temporaires

--- horaires-tarifs-parc.php
+++ horaires-tarifs-parc.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Horaires et tarifs du parc
  * Description: Horaires, calendrier interactif, exceptions, alertes et tarifs multilingues pour les parcs.
- * Version: 1.9.2
+ * Version: 1.9.3
  * Update URI: https://github.com/montagnedessinges/horaires-tarifs-parc
  * Requires at least: 6.0
  * Requires PHP: 7.4
@@ -12,7 +12,7 @@
 
 if (!defined('ABSPATH')) { exit; }
 
-define('PARCS_HT_VERSION', '1.9.2');
+define('PARCS_HT_VERSION', '1.9.3');
 define('PARCS_HT_FILE', __FILE__);
 define('PARCS_HT_DIR', plugin_dir_path(__FILE__));
 define('PARCS_HT_URL', plugin_dir_url(__FILE__));
@@ -22,7 +22,9 @@ require_once PARCS_HT_DIR . 'includes/class-parcs-ht-schedule.php';
 require_once PARCS_HT_DIR . 'includes/class-parcs-ht-bootstrap.php';
 require_once PARCS_HT_DIR . 'includes/class-parcs-ht-slot-last-entry.php';
 require_once PARCS_HT_DIR . 'includes/class-parcs-ht-http-ssl.php';
+require_once PARCS_HT_DIR . 'includes/class-parcs-ht-tariff-seasons.php';
 Parcs_HT_HTTP_SSL::init();
+Parcs_HT_Tariff_Seasons::init();
 
 register_activation_hook(__FILE__, array('Parcs_HT_Defaults', 'activate'));
 register_deactivation_hook(__FILE__, static function () { require_once PARCS_HT_DIR . 'includes/class-parcs-ht-health.php'; Parcs_HT_Health::deactivate(); });
