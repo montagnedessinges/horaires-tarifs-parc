5195e15bda8981008dbc0abdd837cd89bc4c735e
montagnedessinges <montagnedessinges@gmail.com>
2026-08-30 15:56:19 +0200

    1.9.12: activer la nouvelle organisation de l’administration

--- horaires-tarifs-parc.php
+++ horaires-tarifs-parc.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Horaires et tarifs du parc
  * Description: Horaires, calendrier interactif, exceptions, alertes et tarifs multilingues pour les parcs.
- * Version: 1.9.11
+ * Version: 1.9.12
  * Update URI: https://github.com/montagnedessinges/horaires-tarifs-parc
  * Requires at least: 6.0
  * Requires PHP: 7.4
@@ -12,7 +12,7 @@
 
 if (!defined('ABSPATH')) { exit; }
 
-define('PARCS_HT_VERSION', '1.9.11');
+define('PARCS_HT_VERSION', '1.9.12');
 define('PARCS_HT_FILE', __FILE__);
 define('PARCS_HT_DIR', plugin_dir_path(__FILE__));
 define('PARCS_HT_URL', plugin_dir_url(__FILE__));
@@ -27,6 +27,7 @@ require_once PARCS_HT_DIR . 'includes/class-parcs-ht-season-status.php';
 require_once PARCS_HT_DIR . 'includes/class-parcs-ht-group-quotes.php';
 require_once PARCS_HT_DIR . 'includes/class-parcs-ht-quote-languages.php';
 require_once PARCS_HT_DIR . 'includes/class-parcs-ht-quote-gate.php';
+require_once PARCS_HT_DIR . 'includes/class-parcs-ht-admin-groups.php';
 Parcs_HT_HTTP_SSL::init();
 Parcs_HT_Tariff_Seasons::init();
 Parcs_HT_Season_Status::init();
@@ -85,6 +86,7 @@ add_action('plugins_loaded', static function () {
     if (is_admin()) {
         require_once PARCS_HT_DIR . 'includes/class-parcs-ht-admin.php';
         Parcs_HT_Admin::init();
+        Parcs_HT_Admin_Groups::init();
         Parcs_HT_Defaults::maybe_upgrade();
         if (get_option('parcs_ht_tariff_seasons_migrated_193', '0') !== '1') {
             $all = get_option(Parcs_HT_Defaults::OPTION, array());
