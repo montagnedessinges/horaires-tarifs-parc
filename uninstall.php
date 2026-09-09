77913f3ed554b2e38c16854e3f842aac2ab5dc93
montagnedessinges <montagnedessinges@gmail.com>
2026-09-09 17:31:37 +0200

    Supprimer aussi les couleurs Avent lors d’une désinstallation complète

--- uninstall.php
+++ uninstall.php
@@ -30,6 +30,7 @@ foreach (array(
     'parcs_ht_pedagogical_guide_stats_meta',
     'parcs_ht_pedagogical_guide_stats_db_version',
     'parcs_ht_advent',
+    'parcs_ht_advent_appearance',
 ) as $option) {
     delete_option($option);
 }
