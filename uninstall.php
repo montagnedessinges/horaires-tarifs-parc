064476f97399d565c1b42a4ad56c5f6b5c34084a
montagnedessinges <montagnedessinges@gmail.com>
2026-09-12 11:03:29 +0200

    Supprimer les réglages UX Avent à la désinstallation

--- uninstall.php
+++ uninstall.php
@@ -31,6 +31,7 @@ foreach (array(
     'parcs_ht_pedagogical_guide_stats_db_version',
     'parcs_ht_advent',
     'parcs_ht_advent_appearance',
+    'parcs_ht_advent_ux',
 ) as $option) {
     delete_option($option);
 }
